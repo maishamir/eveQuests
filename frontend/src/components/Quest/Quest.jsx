be97d065b54abab5abc2c27f7269a2eb3d8a875d
maishamir <45680002+maishamir@users.noreply.github.com>
2025-03-12 00:52:35 -0700

    basic checkbox UI for task completion

--- frontend/src/components/Quest/Quest.jsx
+++ frontend/src/components/Quest/Quest.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Quest.scss";
 
-function Quest({ task, priority }) {
+function Quest({ task, priority, checkTask, questId, isChecked}) {
+
+  // TODO: add checkbox and set 'checked' state from prop
+  // TODO: on click, call function from QuestBoard and update state
+
   return (
     <div className="quest">
-      <p className="quest__task">{task}</p>
+      <p className={`quest__task ${isChecked ? `quest__complete` : ``}` }>{task}</p>
       <button className="quest__whimsify">whim</button>
-      <input type="checkbox"  />
+      <input type="checkbox" onClick={() => checkTask(questId)} />
     </div>
   );
 }
--- frontend/src/components/QuestBoard/QuestBoard.jsx
+++ frontend/src/components/QuestBoard/QuestBoard.jsx
@@ -1,32 +1,54 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Quest from "../Quest/Quest";
 import "./QuestBoard.scss";
+import { v4 as uuidv4 } from "uuid";
 
 function QuestBoard() {
+  // TODO: create function that updates isChecked state for correct task
+  function completeTask(questId) {
+    // TODO: toggle the isChecked value of the matching task
+    let completedTask = tasks.map(task => {
+     return  task.questId === questId ? {...task, isChecked: !task.isChecked} :  task
+    })
+    setTasks(completedTask)
+  }
+  // TODO: pass function as prop to each component
 
 
   // TODO: use useState to store tasks
-  const [tasks, setTasks] = useState(["Helloo this is the very first task!!", "This is the second task woohooo", "And this is the last one hehe"])
-  // TODO: update the state when adding a task
-  
+  const [tasks, setTasks] = useState([
+    {
+      task: "Helloo this is the very first task!!",
+      isChecked: false,
+      questId: uuidv4(),
+    },
+    {
+      task: "This is the second task woohooo",
+      isChecked: false,
+      questId: uuidv4(),
+    },
+    {
+      task: "And this is the last one hehe",
+      isChecked: false,
+      questId: uuidv4(),
+    },
+  ]);
 
+  // TODO: update the state when adding a task
   function addTask(e) {
     e.preventDefault();
-    setTasks([...tasks, e.target.task.value])
-  }  
-
+    setTasks([...tasks, e.target.task.value]);
+  }
 
   return (
     <div className="questBoard__container">
       <div className="questBoard">
-        
         <div className="questBoard__quests">
-        {/* TODO: render with .map() */}
-          {
-            tasks.map((quest, index) => {
-              return <Quest key={index} task={quest} />
-            })
-          }
+          {tasks.map((task) => {
+            return (
+              <Quest task={task.task} key={task.questId} checkTask={completeTask} questId={task.questId} isChecked={task.isChecked} />
+            );
+          })}
         </div>
 
         <form className="questBoard__addTask" onSubmit={addTask}>
@@ -37,7 +59,9 @@ function QuestBoard() {
             placeholder="Add task here"
           />
           <button className="questBoard__button">Add</button>
-          <button className="questBoard__button questBoard__button--whim">Whim</button>
+          <button className="questBoard__button questBoard__button--whim">
+            Whim
+          </button>
         </form>
       </div>
     </div>
